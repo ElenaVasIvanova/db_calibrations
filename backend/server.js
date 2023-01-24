c4a76e42c8dea8251e5dd3bd38b5727848df2078
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-24 11:31:48 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 const colors = require('colors')
 const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
