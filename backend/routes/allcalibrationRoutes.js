269b4b0ffe3ab50ef11a8bcd8cc16ad7d1998f31
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-24 16:36:33 +0200

    Update allcalibrationRoutes.js

--- backend/routes/allcalibrationRoutes.js
+++ backend/routes/allcalibrationRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const router = express.Router()
 const {getAllCalibrations} = require('../controllers/allcalibrationController')
-router.route('/').get(getAllCalibrations)
+router.route('https://calibration.onrender.com/').get(getAllCalibrations)
 module.exports = router
