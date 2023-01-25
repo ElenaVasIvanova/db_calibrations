dc4fee406486f04c8d14f9ada1ba74a918cde152
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-25 10:54:51 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const path = require('path')
 const PORT = process.env.PORT
 const colors = require('colors')
 const connectDB = require('./config/db')
@@ -23,7 +24,12 @@ app.use(express.urlencoded({ extended: true}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/calibrations', require('./routes/calibrationRoutes'))
 app.use('/api/allcalibrations', require('./routes/allcalibrationRoutes'))
-
+if (process.env.NODE_ENV === 'production'){
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
+	app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else { app.get('/', (req, res) =>{
+	res.status(200)
+})}
 
 app.use(errorHandler)
 app.listen(PORT, () => console.log(colors.green(`Server started on port ${PORT}`))
