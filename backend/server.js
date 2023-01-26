7f0fe62641f284298dd294fc0c6a016aa60bf768
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-26 21:33:42 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,12 @@ app.use(express.urlencoded({ extended: true}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/calibrations', require('./routes/calibrationRoutes'))
 app.use('/api/allcalibrations', require('./routes/allcalibrationRoutes'))
-app.use(errorHandler)
+
 if (process.env.NODE_ENV === 'production'){
 	app.use(express.static(path.join(__dirname, '../frontend/build')))
 	app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
 }
+app.use(errorHandler)
 app.listen(port, () => console.log(colors.green(`Server started on port ${port}`)))
 
 
