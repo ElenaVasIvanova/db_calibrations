fb79382c5473fda44629311c87e36975f33761b9
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-27 12:55:50 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 root.render(
 	<React.StrictMode>
 	<Provider store={store}>
