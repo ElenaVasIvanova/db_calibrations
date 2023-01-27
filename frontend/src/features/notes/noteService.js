d4a20fcdf6e8a66afd6f88867ec6a73f4e8ee62b
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-27 12:25:15 +0200

    Update noteService.js

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = 'https://db-calibrations-loft.onrender.com/REACT_SERVER_URL/api/calibrations/'
+const API_URL = 'REACT_APP_SERVER_URL/api/calibrations/'
 const getNotes = async (calibrationId, token) => {
 	const config = {
 		headers: {
