9a4ea065c1f5920969274d3b176bd6079ced55f7
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-27 13:22:22 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 root.render(
-	console.log('SERVER_URL')
+	
 	<React.StrictMode>
 	<Provider store={store}>
 	<App />
