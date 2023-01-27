7e621bae53f89da645e68aee20d6f73b0e5b3bd5
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-27 12:31:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
 
-app.use('REACT_APP_SERVER_URL/api/users', require('./routes/userRoutes'))
-app.use('REACT_APP_SERVER_URL/api/calibrations', require('./routes/calibrationRoutes'))
-app.use('REACT_APP_SERVER_URL/api/allcalibrations', require('./routes/allcalibrationRoutes'))
+app.use('REACT_APP_SERVER_URL/users', require('./routes/userRoutes'))
+app.use('REACT_APP_SERVER_URL/calibrations', require('./routes/calibrationRoutes'))
+app.use('REACT_APP_SERVER_URL/allcalibrations', require('./routes/allcalibrationRoutes'))
 
 if (process.env.NODE_ENV === 'production'){
 	app.use(express.static(path.join(__dirname, '../frontend/build')))
