795ef963115746c01598ce5393ac7ebcc2612b00
ElenaVasIvanova <59367463+ElenaVasIvanova@users.noreply.github.com>
2023-01-27 09:46:20 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,11 @@ connectDB()
 
 const app = express()
 
-
-app.use(function(req, res, next) 
-{res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "X-Requested-With");
-next(); 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
